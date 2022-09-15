846b0585f3a318cc28349dcad23ca183459b694d
Tomas Rataj <47946352+RattCZ@users.noreply.github.com>
2022-09-15 12:11:39 +0100

    Back to square one

--- src/main/java/com/project/StudentPortal/StudentPortalApplication.java
+++ src/main/java/com/project/StudentPortal/StudentPortalApplication.java
@@ -1,10 +1,5 @@
 package com.project.StudentPortal;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
